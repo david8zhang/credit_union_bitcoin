8d0576468bfc620f9f203f4579a92924320ce569
David Zhang <david8zhang@berkeley.edu>
2016-04-02 22:00:45 -0700

    added create charter route

--- server/controllers/charterController.js
+++ server/controllers/charterController.js
@@ -9,5 +9,31 @@ var database_name_charter = test_database_name_charter;
 
 /* POST A new charter */
 exports.createCharter = function(req, res) {
+	var params = {};
+	params.TableName = database_name_charter;
+	var creator_id = req.body.creator_id;
+	var buy_in_amount = req.body.buy_in_amount;
+	var max_amt_loan_percentage = req.body.max_amt_loan_percentage;
+	var interest_rate = req.body.interest_rate;
+	var repayment_window = req.body.repayment_window;
+	var charter_id = sha1(Math.floor(Date.now()/1000).toString());
 
+	params.Item = {
+		charter_id: charter_id,
+		creator_id: creator_id,
+		buy_in_amount: buy_in_amount,
+		max_amt_loan_percentage: max_amt_loan_percentage,
+		interest_rate: interest_rate,
+		repayment_window: repayment_window
+	}
+	charter = params.Item;
+	docClient.put(params, function(err, data) {
+		if(err) {
+			res.status(400).send(err);
+		} else {
+			var response = {};
+			response.charter = charter;
+			res.status(200).send(charter);
+		}
+	});
 }
--- server/test/charterTest.js
+++ server/test/charterTest.js
@@ -2,20 +2,21 @@ var supertest = require("supertest");
 var should = require("should");
 var server = supertest.agent("http://localhost:8900/api/v1");
 
-// describe("Charter unit tests", function() {
-// 	it("should create a new charter", function(done) {
-// 		server
-// 		.post('/charter/create')
-// 		.send({creator_id: '1', buy_in_amount: '0.001', max_amt_loan_percentage: '10', 
-// 			   interest_rate: '0.005', repayment_window: '20'})
-// 		.expect(200)
-// 		.end(function(err, res) {
-// 			should.equal(res.status, 200);
-// 			should.equal(res.body.charter.creator_id, '1');
-// 			should.equal(res.body.charter.buy_in_amount, '0.001');
-// 			should.equal(res.body.charter.max_amt_loan_percentage, '10');
-// 			should.equal(res.body.charter.interest_rate, '0.005');
-// 			should.equal(res.body.charter.repayment_window, '20');
-// 		})
-// 	})
-// })
+describe("Charter unit tests", function() {
+	it("should create a new charter", function(done) {
+		server
+		.post('/charter/create')
+		.send({creator_id: '1', buy_in_amount: '0.001', max_amt_loan_percentage: '10', 
+			   interest_rate: '0.005', repayment_window: '20'})
+		.expect(200)
+		.end(function(err, res) {
+			should.equal(res.status, 200);
+			should.equal(res.body.charter.creator_id, '1');
+			should.equal(res.body.charter.buy_in_amount, '0.001');
+			should.equal(res.body.charter.max_amt_loan_percentage, '10');
+			should.equal(res.body.charter.interest_rate, '0.005');
+			should.equal(res.body.charter.repayment_window, '20');
+			should.exist(res.body.charter.charter_id);
+		})
+	})
+})
